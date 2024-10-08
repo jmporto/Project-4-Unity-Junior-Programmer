61e2a0e9507d3b0ecd86d6d8e88aa66bbe3e83c6
João Marcelo Silva <jmporto@outlook.com>
2024-10-08 05:15:37 -0300

    [Feature]: Lesson 4.2 - Follow the Player
    2- Create enemy script to follow player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        enemyRb.AddForce((player.transform.position
+            - transform.position).normalized * speed);
+    }
+}
