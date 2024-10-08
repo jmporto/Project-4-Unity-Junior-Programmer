d069069427eb75e12d9c67e0dfb4483f3cea0881
João Marcelo Silva <jmporto@outlook.com>
2024-10-08 04:51:34 -0300

    [Feature]: Lesson 4.1 - Watch Where You`re Going
    4- Rotate the focal point by user input

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed *Time.deltaTime);
     }
 }
