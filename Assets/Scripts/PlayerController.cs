9887e58d560d3f72f55c440597ad509275668edb
João Marcelo Silva <jmporto@outlook.com>
2024-10-08 05:07:13 -0300

    [Feature]: Lesson 4.1 - Watch Where You`re Going
    6- Move in direction of focal point

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     public float speed = 5.0f;
+    private GameObject focalPoint;
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        playerRb.AddForce(forwardInput * speed * focalPoint.transform.forward);
     }
 }
