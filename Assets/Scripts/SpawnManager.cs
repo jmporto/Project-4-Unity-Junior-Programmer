e7901d6fe51b92e583875a8b449b5ab0c8f7a4de
João Marcelo Silva <jmporto@outlook.com>
2024-10-08 06:06:16 -0300

    [Feature]: Lesson 4.4 - For-Loops For Waves
    4- Increase enemyCount with waves

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,10 +8,11 @@ public class SpawnManager : MonoBehaviour
     public GameObject enemyPrefab;
     private float spawnRange = 9;
     public int enemyCount;
+    public int waveNumber = 1;
 
     void Start()
     {
-        SpawnEnemyWave(3);
+        SpawnEnemyWave(waveNumber);
     }
 
     void Update()
@@ -19,7 +20,8 @@ public class SpawnManager : MonoBehaviour
         enemyCount = FindObjectsOfType<Enemy>().Length;
         if (enemyCount == 0)
         {
-            SpawnEnemyWave(1);
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
         }
     }
 
